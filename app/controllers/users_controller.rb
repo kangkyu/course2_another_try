d51a5ccd41abfe61ffb9b4884f6546b4889b8d95
Kang Kyu Lee <kangkyu1111@gmail.com>
2014-06-30 22:57:54 -0700

    add register new user feature

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,14 +1,13 @@
 class UsersController < ApplicationController
   def new
-    @user = User.new
+
   end
 
   def create
-    binding.pry
-
-    @user = User.new(username: params[:username], password: params[:password])
+    @user = User.new(username: params[:username], password: params[:password], password_confirmation: params[:password])
 
     if @user.save
+      session[:user_id] = @user.id
       redirect_to root_path
     else
       render 'new'
